3c33aa2f15781a244966425a3bc992d78507386f
Trojan-Boy <132485511+Trojan-Boy@users.noreply.github.com>
2023-06-28 18:27:12 +0530

    Add files via upload

---
+++ bot4.js
@@ -0,0 +1,84 @@
+
+const botToken = '6292121677:AAEudyLAH2u5OGOQc_R3sy776vENQ3fiNH0';
+const TelegramBot = require('node-telegram-bot-api');
+const QRCode = require('qrcode');
+const fs = require('fs');
+
+// Replace 'YOUR_TELEGRAM_BOT_TOKEN' with your actual bot token
+const bot = new TelegramBot(botToken, { polling: true });
+
+// Handle incoming messages
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(
+        chatId,
+        'Send me a text and I will generate a QR code for you! You can also specify the shape by sending a shape command (e.g., /circle, /triangle, /square).'
+    );
+});
+
+bot.onText(/\/(circle|triangle|square)/, (msg, match) => {
+    const chatId = msg.chat.id;
+    const shape = match[1].toLowerCase();
+    const text = msg.text.replace('/' + shape, '').trim();
+
+    // Generate QR code with specified shape and random color
+    const options = {
+        color: {
+            dark: getRandomColor(),
+            light: getRandomColor()
+        },
+        errorCorrectionLevel: 'H',
+        type: shape
+    };
+
+    generateQRCode(chatId, text, options);
+});
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    // Generate QR code with default shape (square) and random color
+    const options = {
+        color: {
+            dark: getRandomColor(),
+            light: getRandomColor()
+        },
+        errorCorrectionLevel: 'H'
+    };
+
+    generateQRCode(chatId, text, options);
+});
+
+// Helper function to generate QR code
+function generateQRCode(chatId, text, options) {
+    options = options || {};
+
+    QRCode.toFile('qrcode.jpg', text, options, (err) => {
+        if (err) {
+            bot.sendMessage(chatId, 'An error occurred while generating the QR code.');
+            return;
+        }
+
+        // Send the QR code image
+        bot.sendPhoto(chatId, 'qrcode.jpg')
+            .then(() => {
+                // Delete the temporary image file
+                fs.unlinkSync('qrcode.jpg');
+            })
+            .catch((error) => {
+                console.log('Error sending photo:', error);
+                bot.sendMessage(chatId, 'An error occurred while sending the QR code.');
+            });
+    });
+}
+
+// Helper function to generate a random color in hexadecimal format
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
---
+++ bot5.js
@@ -0,0 +1,80 @@
+const TelegramBot = require('node-telegram-bot-api');
+const QRCode = require('qrcode');
+const fs = require('fs');
+const botToken = '6292121677:AAEudyLAH2u5OGOQc_R3sy776vENQ3fiNH0';
+// Replace 'YOUR_TELEGRAM_BOT_TOKEN' with your actual bot token
+const bot = new TelegramBot(botToken, { polling: true });
+
+// Handle incoming messages
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Send me a text, image, or file and I will generate a QR code for it!');
+});
+
+bot.onText(/\/qrtext (.+)/, (msg, match) => {
+    const chatId = msg.chat.id;
+    const text = match[1];
+
+    // Generate QR code for the provided text
+    generateQRCode(chatId, text);
+});
+
+bot.on('photo', (msg) => {
+    const chatId = msg.chat.id;
+
+    // Retrieve the photo file ID
+    const fileId = msg.photo[0].file_id;
+
+    // Get the photo file by ID
+    bot.getFile(fileId).then((fileInfo) => {
+        const fileUrl = `https://api.telegram.org/file/bot${bot.token}/${fileInfo.file_path}`;
+
+        // Generate QR code for the photo
+        generateQRCode(chatId, fileUrl);
+    }).catch((error) => {
+        console.log('Error retrieving photo:', error);
+        bot.sendMessage(chatId, 'An error occurred while retrieving the photo.');
+    });
+});
+
+bot.on('document', (msg) => {
+    const chatId = msg.chat.id;
+
+    // Retrieve the document file ID
+    const fileId = msg.document.file_id;
+
+    // Get the document file by ID
+    bot.getFile(fileId).then((fileInfo) => {
+        const fileUrl = `https://api.telegram.org/file/bot${bot.token}/${fileInfo.file_path}`;
+
+        // Generate QR code for the document
+        generateQRCode(chatId, fileUrl);
+    }).catch((error) => {
+        console.log('Error retrieving document:', error);
+        bot.sendMessage(chatId, 'An error occurred while retrieving the document.');
+    });
+});
+
+// Helper function to generate QR code
+function generateQRCode(chatId, data) {
+    QRCode.toFile('qrcode.jpg', data, (err) => {
+        if (err) {
+            bot.sendMessage(chatId, 'An error occurred while generating the QR code.');
+            return;
+        }
+
+        // Read the generated QR code image
+        const qrCodeImage = fs.readFileSync('qrcode.jpg');
+
+        // Send the QR code image
+        bot.sendPhoto(chatId, qrCodeImage)
+            .then(() => {
+                // Delete the temporary image file
+                fs.unlinkSync('qrcode.jpg');
+            })
+            .catch((error) => {
+                console.log('Error sending photo:', error);
+                bot.sendMessage(chatId, 'An error occurred while sending the QR code.');
+            });
+    });
+}
